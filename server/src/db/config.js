5f2645d6380d5944fc585392ecbf765e172e168f
lugomez92 <lugomez@tripadvisor.com>
2024-05-06 16:58:05 -0400

    added db  sequelize instance

--- server/src/db/config.js
+++ server/src/db/config.js
@@ -1,1 +1,10 @@
 // add your database connection here
+const { Sequelize } = require('sequelize');
+
+//create an instance of this sequelize class
+const db = new Sequelize({
+    dialect: 'sqlite',
+    storage: './my-database.sqlite'
+})
+
+module.exports = { db };
